5f36355ea9ec2ffc7047cedcb74d0d10b798d68c
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-02-12 18:42:09 -0300

    a

--- app.py
+++ app.py
@@ -127,7 +127,7 @@ border= """
 
 border2= """
     <style>
-    [class="bglayer"]
+    [class="st-emotion-cache-keje6w e1f1d6gn3"]
     {
     background-color: black;
     border-radius: 15px 15px;
