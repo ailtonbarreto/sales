3fb777111409976380cdf6233f9317be65fb9df5
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-02-12 17:23:01 -0300

    a

--- app.py
+++ app.py
@@ -20,8 +20,8 @@ pgbg= """
 
 st.markdown(pgbg,unsafe_allow_html=True)
 
-# with open("style.css") as f:
-#     st.markdown(f"<style>{f.read()}</style>",unsafe_allow_html = True)
+with open("style.css") as f:
+    st.markdown(f"<style>{f.read()}</style>",unsafe_allow_html = True)
 
 #----------------------------------------------------------------------------------------------------
 
