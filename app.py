ab56937c09c97b05b24955c9a7bf941fe0a80dbe
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-02-12 18:52:23 -0300

    a

--- app.py
+++ app.py
@@ -57,7 +57,8 @@ cola,colb = st.columns([100,1])
 col1, col2 = st.columns(2)
 col3, col4, col5 = st.columns(3)
 
-cola.st.title(f'Sales Analysis {month} - {year}',anchor=False)
+with cola:
+    st.title(f'Sales Analysis {month} - {year}',anchor=False)
 #----------------------------------------------------------------------------------------------------
 #Graphics
 
