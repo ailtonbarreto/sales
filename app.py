3f32b1ef0b5da5747fdeb01246f096578ee4ad77
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-02-12 18:33:26 -0300

    a

--- app.py
+++ app.py
@@ -127,10 +127,10 @@ border= """
 
 border2= """
     <style>
-    [data-testid="column"]
+    [data-testid="stHorizontalBlock"]
     {
-    background-color: white;
-    border-radius: 15px;
+    background-color: black;
+    border-radius: 15px 15px;
     }
     </style>
 """
