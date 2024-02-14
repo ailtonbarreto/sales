34dbeea935c3a92a1127d40b367b1f0e4bceeb1b
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-02-13 22:07:44 -0300

    a

--- app.py
+++ app.py
@@ -77,7 +77,7 @@ fig_prod = px.bar(df_filtered,
         color="City", title="Product Line",
         orientation="h",
         color_discrete_sequence=['#16db65','#058c42','#04471c'])
-fig_prod.update_xaxes(showgrid=False, visible=True)
+fig_prod.update_xaxes(showgrid=False,showticklabels=False)
 fig_prod.update_yaxes(showgrid=False, visible=True)
 
 
