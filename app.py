e85d98a3befd468f3ba67023befceabb29baf7b3
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-02-15 22:45:04 -0300

    a

--- app.py
+++ app.py
@@ -110,7 +110,7 @@ col5.plotly_chart(fig_rating, use_container_width=True)
 
 borda_graficos2 = """
     <style>
-    [data-testid="stHorizontalBlock"]
+    [data-testid="stFullScreenFrame"]
     {
     border-radius: 15px;
     background-color: white;
