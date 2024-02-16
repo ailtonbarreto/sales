f62aefb24d9fdb614231c94cb1a6e8c1ba5062c0
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-02-16 19:42:23 -0300

    a

--- app.py
+++ app.py
@@ -78,7 +78,7 @@ fig_prod = px.bar(df_filtered,
         orientation="h",
         color_discrete_sequence=['#16db65','#058c42','#04471c'])
 fig_prod.update_xaxes(showgrid=False,visible=True,title="")
-fig_prod.update_yaxes(showgrid=False, visible=True)
+fig_prod.update_yaxes(showgrid=False, visible=True,title="")
 
 
 col2.plotly_chart(fig_prod, use_container_width=True)
