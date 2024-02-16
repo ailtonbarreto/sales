efbe0a73c4b0d5e464baf45c87a313d9506f3444
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-02-15 22:31:20 -0300

    a

--- app.py
+++ app.py
@@ -121,7 +121,7 @@ borda_graficos = """
 
 borda_graficos2 = """
     <style>
-    [class="user-select-none svg-container"]
+    [class="stHorizontalBlock"]
     {
     border-radius: 15px;
     background-color: white;
