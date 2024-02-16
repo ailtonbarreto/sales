cbd65220274dd8133c5060ea226202437b71b749
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-02-16 19:44:34 -0300

    a

--- app.py
+++ app.py
@@ -69,7 +69,6 @@ fig_date = px.bar(df_filtered.groupby(['Day','City'])[["Total"]].sum().reset_ind
         x="Day", y="Total", color="City", title="Daily Sales",
         color_discrete_sequence=['#023e8a','#0a9396','#0077b6'])
 fig_date.update_yaxes(showgrid=False, visible=True,title="")
-fig_date.update_xaxes(showgrid=False,visible=True,title="")
 col1.plotly_chart(fig_date, use_container_width=True)
 
 fig_prod = px.bar(df_filtered, 
