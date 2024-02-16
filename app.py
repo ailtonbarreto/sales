dfef1df4e4f9351315b76affc0a029d692f9d6ca
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-02-15 22:09:07 -0300

    a

--- app.py
+++ app.py
@@ -110,7 +110,7 @@ col5.plotly_chart(fig_rating, use_container_width=True)
 
 borda_graficos = """
     <style>
-    [data-testid="stAppViewBlockContainer"]
+    [data-testid="stHorizontalBlock"]
     {
     border-radius: 15px;
     border-color: white;
