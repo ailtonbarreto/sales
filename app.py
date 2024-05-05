2024703d85ed49a041dfb3722be762cb5b46da87
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-05-04 23:54:32 -0300

    updated

--- app.py
+++ app.py
@@ -178,7 +178,7 @@ hidebordertitle = """
     [class="st-emotion-cache-6vw1hb e1f1d6gn3"]
     {
     background: none;
-    backround-color: none;
+    backface-visibility: hidden;
     }
     </style>
 """
